4a7ea60fc46a46eb5d06831a12972d91eb24a745
Eugene <edegtyarev.evgeny@gmail.com>
2024-05-02 23:04:03 +0300

    Readme

--- TZ1/solution.sh
+++ TZ1/solution.sh
@@ -8,21 +8,40 @@ find "$input_dir" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;
 echo "Список всех вложенных файлов:"
 find "$input_dir" -type f -exec basename {} \;
 
+# получаем список всех вложенных файлов
 files=$(find "$input_dir" -type f)
 echo "Копирование файлов:"
+# перебираем файлы
 for file in $files; do
+    # получаем полное имя файла
     file_full=$(basename "$file")
+    if [[ "$file_full" == *.* ]]; then
+    # отделяем только название файла, без разширения
     file_name="${file_full%.*}"
+    # отделяем расширение файла
     extension="${file_full##*.}"
+    else
+    # если файл без расширения, то просто оставляем переменную с расширением пустой
+    file_name="$file_full"
+    extension=""
+    fi
 
     dir_path=$(dirname "$file")
-    last_folder=$(basename "$dir_path")
-    new_path="${output_dir}/${file_name}.${extension}"
-    if [ -f "$new_path" ]; then
-        new_path="${output_dir}/${file_name}_from_${last_folder}.${extension}"
+    # стороим новый путь, учитывая, что расширение может быть пустым
+    new_path="${output_dir}/${file_name}${extension:+.$extension}"
+    # если файл в выходной папке уже есть, то начинаем прибавлять суффиксы
+    if [[ -f "$new_path" ]]; then
+    suffix=1
+    # начинаем с единицы и добавляем единицу к суффиксу, пока путь еще существует
+    while [[ -f "${output_dir}/${file_name}_${suffix}${extension:+.$extension}" ]]; do
+        ((suffix++))
+    done
+    # в конце прибавляем последний суффикс
+    new_path="${output_dir}/${file_name}_${suffix}${extension:+.$extension}"
 fi
+    # копирование файла в новый путь
     cp "$file" "$new_path"
-    echo "Копирование ${file} завершено"
+    echo "Копирование файла ${file} завершено"
 done
 echo "Список всех файлов в $2:"
 find "$output_dir" -type f -exec basename {} \;
